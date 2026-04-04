44652efb4b54a75f12aca6970507a9b7f8b028ce
Filip Voska <filip.voska@gmail.com>
2026-04-05 00:01:21 +0200

    fix: extend AudioServiceActivity for audio_service compatibility
    MainActivity must extend AudioServiceActivity (not FlutterActivity)
    for audio_service's MediaSession to find the correct FlutterEngine.
    Fixes IllegalStateException crash on Android.

--- android/app/src/main/kotlin/tech/voska/rtsp_mixer/MainActivity.kt
+++ android/app/src/main/kotlin/tech/voska/rtsp_mixer/MainActivity.kt
@@ -1,5 +1,5 @@
 package tech.voska.rtsp_mixer
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
